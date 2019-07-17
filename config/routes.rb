c6d6b66ec4d34a24c5295f2059bbe64e2258a67e
Rachel Hanna <rjoyhanna@gmail.com>
2019-07-17 14:29:02 -0700

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # the string after get can be whatever we want, it doesn't have to match the righthand side
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  
+
   resources :blogs
 
   root to: 'pages#home'
